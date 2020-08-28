6f89b4c2c03871c6ac9efb5b1fe7d6eb77e9cbd8
Christian Effiong <norexus007@gmail.com>
2020-08-28 22:32:29 +0100

    complete Register page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,20 +1,104 @@
 import React from "react";
-import { Box } from "@chakra-ui/core";
+import { useForm } from "react-hook-form";
+import {
+  Box,
+  Link,
+  Image,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Button,
+  Input,
+  Spinner,
+} from "@chakra-ui/core";
+import { Link as RouterLink } from "react-router-dom";
 import Layout from "../Layout";
 import { useDocTitle } from "../useDocTitle";
 
 const Register = () => {
   useDocTitle("Register | Passwordless Authentication");
+
+  const darkMode = JSON.parse(localStorage.getItem("darkMode"));
+
+  const { handleSubmit, errors, register, formState } = useForm();
+
+  const validateName = (value) => {
+    let error;
+    if (!value) {
+      error = "Name is required";
+    } else if (value !== "Naruto") {
+      error = "Jeez! You're not a fan 😱";
+    }
+    return error || true;
+  };
+
+  const onSubmit = (values) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+    }, 1000);
+  };
+
   return (
     <Layout isAuthPage={true}>
       <Box
         width="100%"
         margin="0 auto"
-        maxWidth="36rem"
+        maxWidth={["22rem", "22rem", "25rem", "25rem"]}
         textAlign="center"
-        padding="0 1.5rem"
+        padding="2rem"
+        borderRadius="5px"
+        bg={darkMode ? "gray.900" : "gray.50"}
       >
-        Register Form
+        <Link
+          textAlign="center"
+          as={RouterLink}
+          to="/"
+          _hover={{ textDecoration: "none" }}
+        >
+          <Image
+            size="5rem"
+            src="https://i.postimg.cc/K8ZDTmrb/logo-bg.png"
+            alt="logo"
+            margin="0 auto"
+          />
+        </Link>
+        <form style={{ marginTop: "2rem" }} onSubmit={handleSubmit(onSubmit)}>
+          <FormControl mb={5} textAlign="left" isInvalid={errors.name}>
+            <FormLabel htmlFor="name">Your Name</FormLabel>
+            <Input
+              name="name"
+              id="name"
+              placeholder="John Doe"
+              ref={register({ validate: validateName })}
+            />
+            <FormErrorMessage>
+              {errors.name && errors.name.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl mb={8} textAlign="left" isInvalid={errors.name}>
+            <FormLabel htmlFor="email">Email Address</FormLabel>
+            <Input
+              name="email"
+              id="email"
+              type="email"
+              placeholder="johndoe@domain.com"
+              ref={register({ validate: validateName })}
+            />
+            <FormErrorMessage>
+              {errors.name && errors.name.message}
+            </FormErrorMessage>
+          </FormControl>
+          <Button
+            mt={4}
+            variantColor="teal"
+            isLoading={formState.isSubmitting}
+            type="submit"
+            w="100%"
+            isDisabled={!true}
+          >
+            {!true ? <Spinner size="md" /> : <span>Sign Up</span>}
+          </Button>
+        </form>
       </Box>
     </Layout>
   );
